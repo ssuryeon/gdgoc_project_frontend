8e2040da6f7fd71b67104d50274a03622ca56274
Leesuryeon <lees8351@naver.com>
2025-11-28 15:51:41 +0900

    feat: 채팅 말풍선 추가

--- src/pages/ChattingPage.tsx
+++ src/pages/ChattingPage.tsx
@@ -6,23 +6,47 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import ProfileIcon from '../components/ProfileIcon';
 import CustomText from '../components/CustomText';
 
-const OtherChat = styled.View`
-    padding: 10px;
-    background-color: ${props => props.theme.chat1};
-    border-radius: 10px;
-    position: 'relative';
-    align-items: 'center';
-    &:before {
-        position: 'absolute';
-        top: 13px;
-        left: -100%;
-        width: 0;
-        height: 0;
-        border-style: solid;
-        border-width: 10px 20px 10px 0px;
-        border-color: transparent ${props => props.theme.chat1} transparent transparent;
-    }
+const Triangle = styled.View`
+    border-left-width: 15px;
+    border-right-width: 30px;
+    border-top-width: 15px;
+    border-bottom-width: 15px;
+    border-left-color: transparent;
+    border-right-color: ${props => props.theme.chat1};
+    border-top-color: transparent;
+    border-bottom-color: transparent;
+    width: 0;
+    height: 0;
+    margin-right: -6;
 `;
+
+interface IChat {
+    text: string,
+}
+
+const OtherChat = ({text}:IChat) => {
+    const theme = useTheme();
+    return (
+        <View style={{width: '100%', flexDirection: 'row', marginBottom: 10}}>
+            <Triangle style={{borderStyle: 'solid', top: 7.5}}/>
+            <View style={[styles.other, {backgroundColor: theme.chat1}]}>
+                <CustomText style={{fontSize: 20, fontFamily: 'Pretendard-SemiBold'}}>{text}</CustomText>
+            </View>
+        </View>
+    )
+}
+
+const MyChat = ({text}:IChat) => {
+    const theme = useTheme();
+    return (
+        <View style={{width: '100%', flexDirection: 'row', marginBottom: 10}}>
+            <View style={[styles.other, {backgroundColor: theme.brandColor, marginRight: -6}]}>
+                <CustomText style={{fontSize: 20, fontFamily: 'Pretendard-SemiBold'}}>{text}</CustomText>
+            </View>
+            <Triangle style={{borderStyle: 'solid', top: 7.5, borderLeftColor: theme.brandColor, borderRightColor: 'transparent', borderLeftWidth: 30, borderRightWidth: 15}}/>
+        </View>
+    )
+}
 
 const ChatInput = styled.TextInput`
     flex: 1;
@@ -41,7 +65,7 @@ const InsertChat = () => {
                 <Ionicons name="arrow-up" size={35} color="#fff"/>
             </Pressable>
         </View>
-    )
+    );
 }
 
 
@@ -52,13 +76,16 @@ const ChattingPage = () => {
             <Header text="youngsukim12"/>
             <Pressable style={{position: 'absolute', top: '5%', left: 10, transform: 'translateY(-20px)'}}><Ionicons name='chevron-back-outline' size={45} color='#fff'/></Pressable>
             <ScrollView style={{width: '100%', height: '90%'}}>
-                <Container style={{backgroundColor: theme.inputColor, padding: 15, justifyContent: 'flex-start', minHeight: '100%'}}>
-                    <OtherChat><CustomText style={{fontSize: 20, fontFamily: 'Pretendard-SemiBold'}}>안녕하세요</CustomText></OtherChat>
+                <Container style={{backgroundColor: theme.inputColor, padding: 20, justifyContent: 'flex-start', minHeight: '100%'}}>
+                    <View style={{width: '100%', height: '100%', paddingRight: 40}}>
+                        <OtherChat text='안녕하세요'/>
+                        <MyChat text="안녕하세요"/>
+                    </View>
                 </Container>
             </ScrollView>
             <InsertChat />
         </>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -69,6 +96,11 @@ const styles = StyleSheet.create({
         justifyContent: 'center', 
         alignItems: 'center', 
         marginLeft: 10
+    },
+    other: {
+        padding: 10,
+        borderRadius: 10,
+        width: '100%',
     }
 })
 
